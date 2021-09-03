2de76b9290d2a9424b6162aabb76efbb5964a8d9
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-09-03 02:36:04 -0300

    :tada: (initial commit)

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+export function Main() {
+  return (
+    <div>
+      <h1>React Avançado</h1>
+    </div>
+  );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+
+import GlobalStyles from '~/styles/GlobalStyles';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="images/icons/favicon.ico" />
+        <meta
+          name="description"
+          content="A Simple Project starter to work with TypeScript, React, NextJs and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
