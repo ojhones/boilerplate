0e040e0acfc3162baa6505f9ac865bc749d81691
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-09-20 19:27:46 -0300

    :sparkles: (Added plop for create templates)

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Button from '.';
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta;
+
+export const Basic: Story = (args) => <Button />;
